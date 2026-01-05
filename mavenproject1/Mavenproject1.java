f41f44b83b11b77b67a9078c89cdd75e91d70646
jureny07 <jurenyamani43@gmail.com>
2026-01-05 01:53:57 +0100

    Add files via upload
    GUI

---
+++ mavenproject1/Mavenproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author juren
+ */
+import java.util.Scanner;
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+
+       
+}
+}
